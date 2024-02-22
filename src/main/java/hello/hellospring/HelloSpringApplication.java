a9eb3415251714a17d699c42c10d12e9d23fefa5
Seungjun Kim <zzjoon97@naver.com>
2024-02-22 21:07:00 +0900

    240222

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -2,6 +2,8 @@ package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class HelloSpringApplication {
@@ -9,5 +11,6 @@ public class HelloSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
+
 
 }
